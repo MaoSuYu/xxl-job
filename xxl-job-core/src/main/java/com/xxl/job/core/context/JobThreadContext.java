38b43dfc64e748a04e29da23c9ae8b1ab6dfe7ed
symao <symao@test.com>
2025-03-06 14:49:37 +0800

    添加注释

--- xxl-job-core/src/main/java/com/xxl/job/core/context/JobThreadContext.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/context/JobThreadContext.java
@@ -3,6 +3,10 @@ package com.xxl.job.core.context;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class JobThreadContext {
+
+    /**
+     * 所有执行工作任务的线程都会注册到其中
+     */
     public static ConcurrentHashMap<String, Thread> JOB_THREAD_CONTEXT_MAP = new ConcurrentHashMap<>();
 
     public static ConcurrentHashMap<String, Thread> getJobThreadContextMap() {
